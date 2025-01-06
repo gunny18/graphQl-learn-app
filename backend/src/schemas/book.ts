1f8a0157e3cea6aa93018edc7bd1bfca4cc94ff8
Ganeshswaminathan R <ganeshswaminathanr@gmail.com>
2025-01-07 00:05:26 +0530

    Added few more query tasks

--- backend/src/schemas/book.ts
+++ backend/src/schemas/book.ts
@@ -1,12 +1,43 @@
-import { GraphQLObjectType, GraphQLString } from "graphql";
+import {
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLString,
+} from "graphql";
+import { AUTHORS, BOOKS } from "./mockData";
 
-const BookType = new GraphQLObjectType({
+const BookType: GraphQLObjectType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     title: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        const author = AUTHORS.find((a) => a.id === parent.authorID);
+        if (!author) return null;
+        return author;
+      },
+    },
   }),
 });
 
-export default BookType;
+const AuthorType: GraphQLObjectType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        const books = BOOKS.filter((b) => b.authorID === parent.id);
+        return books;
+      },
+    },
+  }),
+});
+
+export { BookType, AuthorType };
--- backend/src/schemas/index.ts
+++ backend/src/schemas/index.ts
@@ -1,18 +1,32 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
-import BookType from "./book";
-import { BOOKS } from "./mockData";
+import {
+  GraphQLID,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from "graphql";
+import { AUTHORS, BOOKS } from "./mockData";
+import { AuthorType, BookType } from "./book";
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         //code to fetch data from DB/other sources
         const book = BOOKS.find((b) => b.id === args.id);
         if (book) return book;
         return null;
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        const author = AUTHORS.find((a) => a.id === args.id);
+        if (!author) return null;
+        return author;
       },
     },
   },
