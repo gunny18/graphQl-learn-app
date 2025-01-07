5e1fabc2539df4a2b0f7313b03addde66d0c6ab8
Ganeshswaminathan R <ganeshswaminathanr@gmail.com>
2025-01-07 11:26:37 +0530

    Added initial frontend app setup with tailwind and shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Book App</h1>
+      <Button>Click me!</Button>
+    </div>
+  );
 };
 
 export default App;
